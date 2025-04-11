9077419910ca0f1dde2b2a17580460bdecffb47c
Laurent Lahmy <lahmy.laurent@gmail.com>
2025-04-11 14:01:26 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,115 @@
+import {
+  Form,
+  Radio,
+  RadioGroup
+} from "@react-spectrum/s2";
+import "@react-spectrum/s2/page.css";
+import { style } from "@react-spectrum/s2/style" with { type: "macro" }
+
+
+function App() {
+
+  return (
+    <main className={style({
+      height: "screen",
+      overflow: "scroll"
+})}>
+      <Form>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      <RadioGroup>
+  <Radio value="soccer">Soccer</Radio>
+  <Radio value="baseball">Baseball</Radio>
+  <Radio value="football" isDisabled>Football</Radio>
+  <Radio value="basketball">Basketball</Radio>
+        </RadioGroup>
+      </Form>
+        
+
+    </main>
+  );
+}
+
+export default App;
